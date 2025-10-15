3e3c851fa6dd354e4f0c9c7751754d57fc37af8a
julencasazk <julencasazk@gmail.com>
2025-10-15 13:50:18 +0200

    copy code from earlier project

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # __ESP32 Platooning node using micro-ROS__
-
 //TODO
--- main/main.c
+++ main/main.c
@@ -1,6 +1,141 @@
+#include <string.h>
 #include <stdio.h>
+#include <unistd.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#include "esp_log.h"
+#include "esp_system.h"
+#include "driver/uart.h"
+#include "driver/gpio.h"
+
+#include <rcl/rcl.h>
+#include <rcl/error_handling.h>
+#include <std_msgs/msg/float32.h>
+#include <rclc/rclc.h>
+#include <rclc/executor.h>
+
+#include <rmw_microxrcedds_c/config.h>
+#include <rmw_microros/rmw_microros.h>
+#include "esp32_serial_transport.h"
+
+#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Aborting.\n",__LINE__,(int)temp_rc);vTaskDelete(NULL);}}
+#define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Continuing.\n",__LINE__,(int)temp_rc);}}
+
+#define SPEED_THRESHOLD 25.0f
+
+
+bool interrupt_flag = false;
+
+rcl_publisher_t publisher;
+rcl_subscription_t subscriber;
+std_msgs__msg__Float32 msg;
+
+void subscription_callback(const void * msgin)
+{
+	const std_msgs__msg__Float32 * incoming_msg = (const std_msgs__msg__Float32 *)msgin;
+	msg.data = incoming_msg->data;
+    if (msg.data > SPEED_THRESHOLD && !interrupt_flag) {
+        // Trace on GPIO21 when publishing
+        // Lets hope this small pulse registers in the other board's interrupt pin
+        gpio_set_level(GPIO_NUM_21, 1);
+		interrupt_flag = true;
+	} else if (msg.data <= SPEED_THRESHOLD) {
+		interrupt_flag = false;
+    } 
+	
+	RCSOFTCHECK(rcl_publish(&publisher, &msg, NULL));
+        gpio_set_level(GPIO_NUM_21, 0);
+}
+
+
+void micro_ros_task(void * arg)
+{
+
+    // Configure GPIO for timing analysis output
+    gpio_config_t out_conf = {
+        .pin_bit_mask = (1ULL<<GPIO_NUM_21),
+        .mode = GPIO_MODE_OUTPUT,
+        .pull_up_en = GPIO_PULLUP_DISABLE,
+        .pull_down_en = GPIO_PULLDOWN_DISABLE,
+        .intr_type = GPIO_INTR_DISABLE
+    };
+
+	gpio_config(&out_conf);
+
+	rcl_allocator_t allocator = rcl_get_default_allocator();
+	rclc_support_t support;
+
+	// create init_options
+	RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
+
+	// create node
+	rcl_node_t node;
+	RCCHECK(rclc_node_init_default(&node, "speed_esp32_node", "", &support));
+
+	// create publisher
+	RCCHECK(rclc_publisher_init_best_effort(
+		&publisher,
+		&node,
+		ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32),
+		"speed_esp32"));
+
+	// create subscriber
+	RCCHECK(rclc_subscription_init_best_effort(
+		&subscriber,
+		&node,
+		ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32),
+		"speed_debug"));
+
+
+	// create executor
+	rclc_executor_t executor;
+	RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
+	RCCHECK(rclc_executor_add_subscription(
+		&executor,
+		&subscriber,
+		&msg,
+		subscription_callback,
+		ON_NEW_DATA));
+
+	msg.data = 0.0f;
+
+	while(1){
+		rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100));
+		usleep(1);
+	}
+
+	// free resources
+	RCCHECK(rcl_publisher_fini(&publisher, &node));
+	RCCHECK(rcl_subscription_fini(&subscriber, &node));
+	RCCHECK(rcl_node_fini(&node));
+
+  	vTaskDelete(NULL);
+}
+
+static size_t uart_port = UART_NUM_0;
 
 void app_main(void)
 {
+#if defined(RMW_UXRCE_TRANSPORT_CUSTOM)
+	rmw_uros_set_custom_transport(
+		true,
+		(void *) &uart_port,
+		esp32_serial_open,
+		esp32_serial_close,
+		esp32_serial_write,
+		esp32_serial_read
+	);
 
+#else
+#error micro-ROS transports misconfigured
+#endif  // RMW_UXRCE_TRANSPORT_CUSTOM
+
+    xTaskCreate(micro_ros_task,
+            "uros_task",
+            CONFIG_MICRO_ROS_APP_STACK,
+            NULL,
+            CONFIG_MICRO_ROS_APP_TASK_PRIO,
+            NULL);
 }
