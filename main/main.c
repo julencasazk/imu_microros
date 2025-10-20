d284e2e2c003e465ec33686c3744e66c61d90b00
julencasazk <julencasazk@gmail.com>
2025-10-20 11:36:00 +0200

    simplify pub/sub code and reorder so it works properly

--- main/main.c
+++ main/main.c
@@ -12,6 +12,7 @@
 #include <rcl/rcl.h>
 #include <rcl/error_handling.h>
 #include <std_msgs/msg/float32.h>
+#include <std_msgs/msg/bool.h>
 #include <rclc/rclc.h>
 #include <rclc/executor.h>
 
@@ -20,7 +21,8 @@
 #include "esp32_serial_transport.h"
 
 #define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Aborting.\n",__LINE__,(int)temp_rc);vTaskDelete(NULL);}}
-#define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Continuing.\n",__LINE__,(int)temp_rc);}}
+//#define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Continuing.\n",__LINE__,(int)temp_rc);}}
+#define RCSOFTCHECK(fn) (void)(fn)
 
 #define SPEED_THRESHOLD 25.0f
 
@@ -29,40 +31,68 @@ bool interrupt_flag = false;
 
 rcl_publisher_t throttle_publisher;
 rcl_publisher_t brake_publisher;
-rcl_subscription_t speed_subscriber;
+rcl_publisher_t steer_publisher;
+rcl_publisher_t reverse_publisher;
 
-std_msgs__msg__Float32 msg;
+rcl_subscription_t throttle_subscriber;
+rcl_subscription_t brake_subscriber;
+rcl_subscription_t steer_subscriber;
+rcl_subscription_t reverse_subscriber;
 
-void subscription_callback(const void * msgin)
+std_msgs__msg__Float32 throttle_msg;
+std_msgs__msg__Float32 brake_msg;
+std_msgs__msg__Float32 steer_msg;
+std_msgs__msg__Bool reverse_msg;
+
+static size_t uart_port = UART_NUM_0;
+
+void throttle_cb(const void * msgin)
 {
-	const std_msgs__msg__Float32 * incoming_msg = (const std_msgs__msg__Float32 *)msgin;
-	msg.data = incoming_msg->data;
+//	const std_msgs__msg__Float32 * incoming_msg = (const std_msgs__msg__Float32 *)msgin;
+//	throttle_msg.data = incoming_msg->data;
+	RCSOFTCHECK(rcl_publish(&throttle_publisher, msgin, NULL));
+}
+
+void brake_cb(const void * msgin)
+{
+//	const std_msgs__msg__Float32 * incoming_msg = (const std_msgs__msg__Float32 *)msgin;
+//	brake_msg.data = incoming_msg->data;
+	RCSOFTCHECK(rcl_publish(&brake_publisher, msgin, NULL));
+}
+
+void steer_cb(const void * msgin)
+{
+//	const std_msgs__msg__Float32 * incoming_msg = (const std_msgs__msg__Float32 *)msgin;
+//	steer_msg.data = incoming_msg->data;
+	RCSOFTCHECK(rcl_publish(&steer_publisher, msgin, NULL));
+}
+
+void reverse_cb(const void * msgin)
+{
+//	const std_msgs__msg__Bool * incoming_msg = (const std_msgs__msg__Bool *)msgin;
+//	reverse_msg.data = incoming_msg->data;
+	RCSOFTCHECK(rcl_publish(&reverse_publisher, msgin, NULL));
 }
 
 
-void control_task(void *arg)
-{
-	while(1){
-		std_msgs__msg__Float32 brake_msg;
-		std_msgs__msg__Float32 throttle_msg;
-		if (msg.data < SPEED_THRESHOLD) {
-			brake_msg.data = 0.0f; 
-			throttle_msg.data = 1.0f; 
-		} else {
-			brake_msg.data = 1.0f;
-			throttle_msg.data = 0.0f;
-		}
-		RCSOFTCHECK(rcl_publish(&brake_publisher, &brake_msg, NULL));
-		RCSOFTCHECK(rcl_publish(&throttle_publisher, &throttle_msg, NULL));
-		usleep(100);
-	}
-
-	vTaskDelete(NULL);
-}
 
 
 void micro_ros_task(void * arg)
 {
+	// Set custom transport (In this case UART through USB-to-UART Bridge)
+	rmw_uros_set_custom_transport(
+		true,
+		(void *) &uart_port,
+		esp32_serial_open,
+		esp32_serial_close,
+		esp32_serial_write,
+		esp32_serial_read
+	);
+
+	// Wait for the micro-ROS Agent on the host computer to initialize
+	while (rmw_uros_ping_agent(1000, 1) != RMW_RET_OK) {
+		vTaskDelay(pdMS_TO_TICKS(100));
+	}
 
 	rcl_allocator_t allocator = rcl_get_default_allocator();
 	rclc_support_t support;
@@ -74,70 +104,138 @@ void micro_ros_task(void * arg)
 	rcl_node_t node;
 	RCCHECK(rclc_node_init_default(&node, "platooning_control_esp32_micro_ros", "", &support));
 
-	// create publisher
-	RCCHECK(rclc_publisher_init_best_effort(
+	// PUBLISHERS
+	// --------------------------------------
+	(void)rclc_publisher_init_best_effort(
 		&throttle_publisher,
 		&node,
 		ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32),
-		"throttle_cmd"));
+		"throttle_cmd"
+	);
 
-	RCCHECK(rclc_publisher_init_best_effort(
+	(void)rclc_publisher_init_best_effort(
 		&brake_publisher,
 		&node,
 		ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32),
-		"brake_cmd"));
+		"brake_cmd"
+	);
 
-	// create subscriber
-	RCCHECK(rclc_subscription_init_best_effort(
-		&speed_subscriber,
+	(void)rclc_publisher_init_best_effort(
+		&steer_publisher,
 		&node,
 		ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32),
-		"speed_debug"));
+		"steer_cmd"
+	);
+	
+	(void)rclc_publisher_init_best_effort(
+		&reverse_publisher,
+		&node,
+		ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Bool),
+		"reverse_cmd"
+	);
+	//----------------------------
+	// END PUBLISHERS
 
+	// SUBSCRIPTIONS
+	// ----------------------------
+
+	RCCHECK(rclc_subscription_init_best_effort(
+		&throttle_subscriber,
+		&node,
+		ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32),
+		"throttle_debug"
+	));
+	
+	RCCHECK(rclc_subscription_init_best_effort(
+		&brake_subscriber,
+		&node,
+		ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32),
+		"brake_debug"
+	));
+
+	RCCHECK(rclc_subscription_init_best_effort(
+		&steer_subscriber,
+		&node,
+		ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32),
+		"steer_debug"
+	));
+
+	RCCHECK(rclc_subscription_init_best_effort(
+		&reverse_subscriber,
+		&node,
+		ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Bool),
+		"reverse_debug"
+	));
+
+	// ---------------------------------
+	// END SUBSCRIPTIONS
+
+	// Initialize messages
+	memset(&throttle_msg, 0, sizeof(throttle_msg));
+	memset(&brake_msg, 0, sizeof(brake_msg));
+	memset(&steer_msg, 0, sizeof(steer_msg));
+	memset(&reverse_msg, 0, sizeof(reverse_msg));
 
 	// create executor
 	rclc_executor_t executor;
-	RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
+	RCCHECK(rclc_executor_init(&executor, &support.context, 4, &allocator));
 	RCCHECK(rclc_executor_add_subscription(
 		&executor,
-		&speed_subscriber,
-		&msg,
-		subscription_callback,
-		ON_NEW_DATA));
+		&throttle_subscriber,
+		&throttle_msg,
+		throttle_cb,
+		ON_NEW_DATA
+	));
+	RCCHECK(rclc_executor_add_subscription(
+		&executor,
+		&brake_subscriber,
+		&brake_msg,
+		brake_cb,
+		ON_NEW_DATA
+	));
+	RCCHECK(rclc_executor_add_subscription(
+		&executor,
+		&steer_subscriber,
+		&steer_msg,
+		steer_cb,
+		ON_NEW_DATA
+	));
+	RCCHECK(rclc_executor_add_subscription(
+		&executor,
+		&reverse_subscriber,
+		&reverse_msg,
+		reverse_cb,
+		ON_NEW_DATA
+	));
 
-	msg.data = 0.0f;
+
 
 	while(1){
-		rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100));
-		usleep(100);
+		
+		RCCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(5)));
+		vTaskDelay(pdMS_TO_TICKS(1));
 	}
 
 	// free resources
 	RCCHECK(rcl_publisher_fini(&throttle_publisher, &node));
 	RCCHECK(rcl_publisher_fini(&brake_publisher, &node));
-	RCCHECK(rcl_subscription_fini(&speed_subscriber, &node));
+	RCCHECK(rcl_publisher_fini(&steer_publisher, &node));
+	RCCHECK(rcl_publisher_fini(&brake_publisher, &node));
+
+	RCCHECK(rclc_executor_fini(&executor));
 	RCCHECK(rcl_node_fini(&node));
 
   	vTaskDelete(NULL);
 }
 
-static size_t uart_port = UART_NUM_0;
+
 
 void app_main(void)
 {
-#if defined(RMW_UXRCE_TRANSPORT_CUSTOM)
-	rmw_uros_set_custom_transport(
-		true,
-		(void *) &uart_port,
-		esp32_serial_open,
-		esp32_serial_close,
-		esp32_serial_write,
-		esp32_serial_read
-	);
+	
+	esp_log_level_set("*", ESP_LOG_NONE);
 
-#else
-#error micro-ROS transports misconfigured
-#endif  // RMW_UXRCE_TRANSPORT_CUSTOM
+#if defined(RMW_UXRCE_TRANSPORT_CUSTOM)
 
     xTaskCreate(micro_ros_task,
             "uros_task",
@@ -145,12 +243,9 @@ void app_main(void)
             NULL,
             CONFIG_MICRO_ROS_APP_TASK_PRIO,
             NULL);
+#else
+#error micro-ROS transports misconfigured
+#endif  // RMW_UXRCE_TRANSPORT_CUSTOM
 
-	usleep(2000000); // Sleep for 2 seconds to let micro-ROS initialize
-	xTaskCreate(control_task,
-			"control_task",
-			4096,
-			NULL,
-			10,
-			NULL);
+
 }
